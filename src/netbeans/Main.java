a4f81777da8e3a7e8553760f7911ef77fb75541e
Romain LE BRETON <le_breton.romain@courrier.uqam.ca>
2018-02-01 20:57:23 -0500

    Modification Projet NetBeans

---
+++ src/netbeans/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeans;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Tixeno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            BookFile.read();
+        } catch (IOException ex) {
+            System.out.println("Erreur : " + ex.getLocalizedMessage());
+        }
+    }
+}
